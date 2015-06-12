b2b257e1c47b7d8e5f859c8a76ad57a757076f74
Bryan <casamfiml@gmail.com>
2015-06-11 20:52:14 -0500

    Pasos para estabilizar

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-//var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running on port:" + app.get('port'))
-})
+  console.log('Node app is running on port', app.get('port'));
+});
