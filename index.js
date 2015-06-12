1dcd3f352f39b269393a6a979509453a674f360d
Bryan <casamfiml@gmail.com>
2015-06-11 20:42:36 -0500

    more changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 
 app.set('port', (process.env.PORT || 5000));
 
